f3b6e2ea2b62f843b10f144cc51520823c644167
spedmond <114600476+spedmond@users.noreply.github.com>
2023-12-08 10:38:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,40 @@
+let names = [0];
+
 fetch('https://jsonplaceholder.typicode.com/posts')
-      .then(response => response.json())
-      .then(json=>{
-        const posts = json.map(post => {
+    .then(response => response.json())
+    .then(json => {
+    const posts = json.map(post => {
+        return `
+        <div>
+            <h2>${post.title}</h2>
+            <p id=${post.userId}>${post.body}</p>
+            <p>${post.userId}</p>
+        </div>
+        `
+    }).join("")
+
+    document.getElementById("posts").innerHTML = posts;
+    })
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(json => {
+        const users = json.map(user => {
+            names.append(user.username);
             return `
             <div>
-                <h2>${post.title}</h2>
-                <p>${post.body}</p>
-                <p>${post.userId}</p>
+                <h2>${user.name}</h2>
+                <p>@${user.username}</p>
             </div>
             `
         })
-        document.getElementById("posts").innerHTML = posts;
-        })
-      
+
+        for (let i=1; i<=10; i++) {
+            document.getElementById(`${i}`).textContent=names[i];
+        }
+    })
+
+
+    for (let i=1; i<=10; i++) {
+        document.getElementById(i).textContent=names[i];
+    }
